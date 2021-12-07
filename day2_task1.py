787bcbf8b47a2c32f2add3feac05a200b7954d24
Frans Kemp <f.p.kemp@outlook.com>
2021-12-07 11:30:57 +0100

    solved task 1 and 2 of day 2

---
+++ day2_task1.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+h_pos = 0
+v_pos = 0
+
+commands = np.loadtxt("input_day2.txt", dtype=str, delimiter="\n")
+for command in commands:
+    command = command.split()
+    direction = command[0]
+    magnitude = int(command[1])
+    if direction == "forward":
+        h_pos += magnitude
+    elif direction == "down":
+        v_pos += magnitude
+    elif direction == "up":
+        v_pos -= magnitude
+    else:
+        print("something wrong with the command:", command)
+
+print("\n horizontal postion:", h_pos, "\n vertical position:", v_pos)
+multiplication = h_pos * v_pos
+print("multiplication result:", multiplication)
