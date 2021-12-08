7c29c65217789c8ae63aa88652683a2457dce3e9
Frans Kemp <f.p.kemp@outlook.com>
2021-12-08 17:17:19 +0100

    solved task 1 and 2 of day 6

---
+++ day6_task1.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+n_days = 80
+
+
+# first try
+fish_ages_today = np.loadtxt("input_day6.txt", dtype=int, delimiter=",")
+for day in range(n_days):
+    # print("day", day, len(fish_ages_today), "fishes")
+    next_day = []
+    for fish_age in fish_ages_today:
+        if fish_age > 0:
+            next_day.append(fish_age - 1)
+        elif fish_age == 0:
+            next_day.extend([6, 8])
+    fish_ages_today = next_day
+print("final number of fishes:", len(fish_ages_today))
+
+# second try
+fish_ages_today = np.loadtxt("input_day6.txt", dtype=int, delimiter=",")
+for day in range(n_days):
+    # print("day", day, len(fish_ages_today), "fishes")
+    try:
+        unique, counts = np.unique(fish_ages_today, return_counts=True)
+        occurences = dict(zip(unique, counts))
+        fish_ages_today = np.append(fish_ages_today, 9*np.ones(occurences[0]))
+    except KeyError:
+        pass
+    fish_ages_today = np.where(fish_ages_today > 0, fish_ages_today - 1, 6)
+print("final number of fishes:", len(fish_ages_today))
+
+# third try
+starting_fishes = np.loadtxt("input_day6.txt", dtype=int, delimiter=",")
+fish_ages_today = np.array([0])
+n_fish = np.empty((9, 1))
+for day in range(n_days):
+    # print("day", day, len(fish_ages_today), "fishes")
+    try:
+        unique, counts = np.unique(fish_ages_today, return_counts=True)
+        occurences = dict(zip(unique, counts))
+        fish_ages_today = np.append(fish_ages_today, 9*np.ones(occurences[0]))
+    except KeyError:
+        pass
+    fish_ages_today = np.where(fish_ages_today > 0, fish_ages_today - 1, 6)
+    n_fish = np.append(n_fish[1:], len(fish_ages_today))
+
+n_fish = np.flip(n_fish)
+unique, counts = np.unique(starting_fishes, return_counts=True)
+occurences = dict(zip(unique, counts))
+final_n_fishes = 0
+for key in occurences:
+    final_fishes_key = int(n_fish[key] * occurences[key])
+    final_n_fishes += final_fishes_key
+print("final number of fishes:", final_n_fishes)
---
+++ day6_task2.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+n_days = 256
+max_calc_days = 100
+max_fish_age = 8
+mfa = max_fish_age + 1
+
+
+def get_histogram(population):
+    unique, counts = np.unique(population, return_counts=True)
+    hist_in_dict = dict(zip(unique, counts))
+    histogram = np.zeros(mfa)
+    for k in range(mfa):
+        try:
+            histogram[k] = hist_in_dict[k]
+        except KeyError:
+            histogram[k] = 0
+    return histogram
+
+
+def histograms_after_n_days(days):
+    fish_ages_today = np.array([0])
+    histograms_last_days = np.identity(mfa)
+    for day in range(days):
+        # if day % 50 == 0:
+        #     print("day", day)
+        try:
+            occurrences = get_histogram(fish_ages_today)
+            fish_ages_today = np.append(fish_ages_today, mfa*np.ones(int(occurrences[0])))
+        except KeyError:
+            pass
+        fish_ages_today = np.where(fish_ages_today > 0, fish_ages_today - 1, 6)
+        histograms_last_days = np.vstack((histograms_last_days[1:], get_histogram(fish_ages_today)))
+    histograms_last_days = np.flip(histograms_last_days, axis=0)
+    return histograms_last_days
+
+
+def combine_histograms(start_histogram, historical_histograms):
+    key_histograms = np.zeros((mfa, mfa))
+    for key in range(mfa):
+        try:
+            key_histogram = (start_histogram[key] * bar for bar in historical_histograms[key])
+            key_histograms[key] = np.array(list(key_histogram))
+        except IndexError:
+            key_histograms[key] = np.array(list(0 for _ in historical_histograms[key]))
+    final_histogram = np.sum(key_histograms, axis=0)
+    return final_histogram
+
+
+current_population = np.loadtxt("input_day6.txt", dtype=int, delimiter=",")
+population_histogram = get_histogram(current_population)
+age_histograms = histograms_after_n_days(max_calc_days)
+repetitions = n_days // max_calc_days
+rest = n_days % max_calc_days
+with np.printoptions(precision=0):
+    for i in range(repetitions):
+        print(f"population after {i * max_calc_days} days: {np.sum(population_histogram)} {population_histogram}")
+        population_histogram = combine_histograms(population_histogram, age_histograms)
+    print(f"population after {repetitions * max_calc_days} days: {np.sum(population_histogram)} {population_histogram}")
+    final_histograms = histograms_after_n_days(rest)
+    final_population = combine_histograms(population_histogram, final_histograms)
+    print(f"population after {n_days} days: {np.sum(final_population)}, {final_population}")
+    print("\n final population:", int(np.sum(final_population)))
